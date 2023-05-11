3d1fb256d2f31dde0eca208f0e1b57e58b77d616
André Moreira <andresilmor@outlook.com>
2023-05-11 17:53:10 +0100

    Revert "TRF"
    This reverts commit ede18eabdf6d9e62926eee20c7016ae3f349c478.

--- carexr-webapp/src/index.js
+++ carexr-webapp/src/index.js
@@ -5,30 +5,19 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "react-auth-kit";
 
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://54.229.220.82/api/",
-  cache: new InMemoryCache()
-});
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    <ApolloProvider client={client}>
-      <AuthProvider
-        authType={"cookie"}
-        authName={"_auth"}
-        cookieDomain={window.location.hostname}
-        cookieSecure={false}
-      >
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </ApolloProvider>
+    <AuthProvider
+      authType={"cookie"}
+      authName={"_auth"}
+      cookieDomain={window.location.hostname}
+      cookieSecure={false}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
-  
 );
 
